c0b913f9f3ac9ef41418a974e33461fc734da34f
zhid0399123 <zhid0399123@gmail.com>
2024-03-25 21:14:17 +0300

    feat: plugins

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,16 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+  { "nvim-tree/nvim-web-devicons", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "tpope/vim-fugitive" },
+  { "mbbill/undotree" },
+}
