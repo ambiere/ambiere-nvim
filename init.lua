f6206dafd4dd3cd2d2999ee4e5cd4889af1e54cf
zhid0399123 <zhid0399123@gmail.com>
2024-03-25 20:55:28 +0300

    feat: init file

---
+++ init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+require("config.noice")
+require("config.neo-tree")
+require("config.lualine")
+require("config.tailwindcss")
+require("config.telescope")
