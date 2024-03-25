c9c0bf06f9a3bb527b1e628703a08eb897cebf74
zhid0399123 <zhid0399123@gmail.com>
2024-03-25 21:12:03 +0300

    noice setup

---
+++ lua/config/noice.lua
@@ -0,0 +1,9 @@
+require("noice").setup({
+  cmdline = {
+    enabled = false, -- enables the Noice cmdline UI
+    view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+  },
+  messages = {
+    enabled = false, -- enables the Noice messages UI
+  },
+})
