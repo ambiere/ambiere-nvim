e488cab1204d98d53976b066916f8c670faa6321
zhid0399123 <zhid0399123@gmail.com>
2024-04-27 21:33:04 +0300

    set lualine theme
    Signed-off-by: zhid0399123 <zhid0399123@gmail.com>

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,7 +1,7 @@
 require("lualine").setup({
   options = {
     icons_enabled = false,
-    theme = "rose-pine",
+    theme = "candle-grey",
     component_separators = "",
     section_separators = "",
   },
