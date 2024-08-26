27b9a4fdffe0c233ce3d2f0786bb1e9de1716681
zhid0399123 <zhid0399123@gmail.com>
2024-08-26 20:38:42 +0300

    config: color-picker

---
+++ lua/config/colorPicker.lua
@@ -0,0 +1,11 @@
+return {
+  ["icons"] = { "∎", "∎" },
+  ["border"] = "none",
+  ["keymap"] = {
+    ["U"] = "<Plug>ColorPickerSlider5Decrease",
+    ["O"] = "<Plug>ColorPickerSlider5Increase",
+  },
+  ["background_highlight_group"] = "Normal",
+  ["border_highlight_group"] = "",
+  ["text_highlight_group"] = "Normal",
+}
