f57d6843b340ebcc74bff8db81b8ca5b099a3594
zhid0399123 <zhid0399123@gmail.com>
2024-08-26 20:40:47 +0300

    custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,57 +1,39 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymapc.lua
--- Add any additional keymaps here
+local set = vim.keymap.set
 
-local map = vim.keymap.set
+-- clipboard
+set("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
+set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard +\\n" })
+set("x", "<leader>p", [["_dP]], { desc = "Paste yanked text over and over again" })
+set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without register" })
 
-map("n", "<leader>pv", vim.cmd.Ex, { desc = "Preview" })
-map({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to clipboard +\\n" })
-map("n", "<leader>Y", [["+Y]], { desc = "Yank to clipboard" })
-map("x", "<leader>p", [["_dP]], { desc = "Paste yanked text over and over again" })
-map({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without register" })
+-- move line(s)
+set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
+set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
 
-map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move lines up" })
-map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down" })
+-- break and join line(s)
+set("n", "J", "mzJ`z", { desc = "Joins the current line with the line below it" })
+set("n", "<A-b>", "i<CR><Esc>", { desc = "Break the current line at cursor position" })
 
-map("n", "<A-b>", "i<CR><Esc>", { desc = "Break the current line at cursor position" })
-map("n", "J", "mzJ`z", { desc = "Joins the current line with the line below it" })
-map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down by half page" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up by half page" })
-map("n", "n", "nzzzv", { desc = "Next occurrence of the search pattern" })
-map("n", "N", "Nzzzv", { desc = "Previous occurrence of the search pattern" })
+-- page scroll
+set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down by half page" })
+set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up by half page" })
 
-map("i", "<C-c>", "<Esc>")
-map("i", "<C-s>", function()
-  vim.cmd("w")
-end, { desc = "Write file" })
+-- seacrh pattern
+set("n", "n", "nzzzv", { desc = "Next occurrence of the search pattern" })
+set("n", "N", "Nzzzv", { desc = "Previous occurrence of the search pattern" })
 
-map("n", "Q", "<nop>")
-map("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-map("n", "<leader>f", vim.lsp.buf.format)
+set("n", "Q", "<nop>") -- do nothing when q is pressed
 
-map("n", "<C-k>", "<cmd>cnext<CR>zz")
-map("n", "<C-j>", "<cmd>cprev<CR>zz")
-map("n", "<leader>k", "<cmd>lnext<CR>zz", { desc = "Next quick fix" })
-map("n", "<leader>j", "<cmd>lprev<CR>zz", { desc = "Previous quick fix" })
+set("n", "<leader>f", vim.lsp.buf.format) -- format buffer using lsp
+set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<cr>") -- run tmux-sessionizer script
+set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<cr>", { desc = "Cellular Automaton" })
+set("n", "<leader>G", "<cmd>silent !lazy-dawg<cr>", { desc = "lazygit cwd" }) -- run lazygit script
+set("n", "<leader>x+", "<cmd>!chmod +x %<cr>", { silent = true, desc = "Make file executable" }) -- make file executable
+set("n", "<leader><leader>", "<cmd>so<cr>", { desc = "Source file" }) -- source current file
 
-map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-map("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- UndotreeToggle
+set("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
 
-map("n", "<leader>ee", "oif (err) {<CR>}<Esc>Oreturn err<Esc>", { desc = "Insert err block" })
-map("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Automaton" })
-
-map("n", "<leader><leader>", function()
-  vim.cmd("so")
-end, { desc = "Source file" })
-map("n", "<leader>u", vim.cmd.UndotreeToggle, { desc = "Toggle undotree" })
-
-map("n", "<leader>cp", ":Colortils picker<CR>", { desc = "Open color picker" })
-map("n", "<leader>cpl", ":Colortils lighten<CR>", { desc = "Open color picker lighten" })
-map("n", "<leader>cpd", ":Colortils darken<CR>", { desc = "Open color picker darken" })
-map("n", "<leader>cpg", ":Colortils greyscale<CR>", { desc = "Open color picker greyscale" })
-map("n", "<leader>cpc", ":Colortils css list<CR>", { desc = "Open color picker css list" })
-
-local opts = { noremap = true, silent = true }
-
-map("n", "<C-c>", "<cmd>PickColor<cr>", opts)
-map("i", "<C-c>", "<cmd>PickColorInsert<cr>", opts)
+-- color picker keymaps
+set("n", "<C-c>", "<cmd>PickColor<cr>", { noremap = true, silent = true })
+set("i", "<C-c>", "<cmd>PickColorInsert<cr>", { noremap = true, silent = true })
