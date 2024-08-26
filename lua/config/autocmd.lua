7927535107bf66e9278e5854e132103c02373ab0
zhid0399123 <zhid0399123@gmail.com>
2024-08-26 20:37:47 +0300

    autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -4,18 +4,22 @@ local autocmd = vim.api.nvim_create_autocmd
 local ambiereGroup = augroup("Ambiere", {})
 local yankGroup = augroup("HighlightYank", {})
 
+-- remove whitespace at the end of
+-- each line before the buffer is written
 autocmd("BufWritePre", {
   group = ambiereGroup,
   pattern = "*",
   command = [[%s/\s\+$//e]],
 })
 
+-- remove semicolon on js and jsx files
 autocmd("BufWritePre", {
   group = ambiereGroup,
   pattern = "*.{js,jsx}",
   command = [[%s/;$//e]],
 })
 
+-- highlight yanked text for 40ms
 autocmd("TextYankPost", {
   group = yankGroup,
   pattern = "*",
